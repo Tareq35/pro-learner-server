6188263a7e2abb9e7e456429bb05909a27242cbb
Tareq35 <a.t.tariqulislam@gmail.com>
2022-10-28 21:43:13 +0600

    courses api created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 
 
 const coursesCategory = require('./data/coursesCategory.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Courses API Running')
@@ -11,7 +12,11 @@ app.get('/', (req, res) => {
 
 app.get('/courses-category', (req, res) => {
     res.send(coursesCategory)
-})
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+});
 
 app.listen(port, () => {
      console.log('Pro Learner server running on port', port);
