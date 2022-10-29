d4eae9bbb4fc095689a3fb97328d612de9c69fb3
Tareq35 <a.t.tariqulislam@gmail.com>
2022-10-29 12:20:54 +0600

    course filter added

--- index.js
+++ index.js
@@ -16,10 +16,21 @@ app.get('/courses-category', (req, res) => {
     res.send(coursesCategory)
 });
 
+app.get('/courses-category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '07') {
+        res.send(courses)
+    }
+    else {
+        const category_courses = courses.filter(c => c.categoryId === id);
+        res.send(category_courses);
+    }
+})
+
 app.get('/courses', (req, res) => {
     res.send(courses)
 });
 
 app.listen(port, () => {
-     console.log('Pro Learner server running on port', port);
-    });
+    console.log('Pro Learner server running on port', port);
+});
